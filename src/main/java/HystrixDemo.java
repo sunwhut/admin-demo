9dc7e14fa0a1bcf6a8154e682d5e2a755e8bd21f
sunwhut <sunwhut333@gmail.com>
2019-04-07 19:26:40 +0800

    add hystrix demo

---
+++ src/main/java/HystrixDemo.java
@@ -0,0 +1,35 @@
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+
+import java.util.concurrent.TimeUnit;
+
+public class HystrixDemo {
+    public static void main(String[] args) {
+        HelloWorldCommand command = new HelloWorldCommand("helloworld");
+        String result = command.execute();
+        System.out.println("thread: " + Thread.currentThread().getName() + ", result: " + result);
+    }
+}
+
+class HelloWorldCommand extends HystrixCommand<String>{
+    private String name;
+
+    public HelloWorldCommand(String name){
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("HelloWorldGroup"))
+                        .andCommandKey(HystrixCommandKey.Factory.asKey("HelloWorldCommand"))
+                        .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(500)));
+        this.name = name;
+    }
+
+    protected String run() throws Exception {
+        TimeUnit.MILLISECONDS.sleep(1000);
+        return "hystrix thread: " + Thread.currentThread().getName();
+    }
+
+    @Override
+    protected String getFallback() {
+        return name + " executed failed";
+    }
+}
